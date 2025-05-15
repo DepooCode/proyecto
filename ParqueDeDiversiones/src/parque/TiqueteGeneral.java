6cab025c42b411a376d6b1349319b64b728de302
Juanromero011 <168579421+Juanromero011@users.noreply.github.com>
2025-05-14 21:08:58 -0500

    Update TiqueteGeneral.java

--- ParqueDeDiversiones/src/parque/TiqueteGeneral.java
+++ ParqueDeDiversiones/src/parque/TiqueteGeneral.java
@@ -1,19 +1,27 @@
 package parque;
 
+import persistencia.Persistable;
 import java.util.Date;
 
+public class TiqueteGeneral extends Tiquete implements Persistable {
 
-
-
-public class TiqueteGeneral extends Tiquete {
-    
     public TiqueteGeneral(String id, CategoriaTiquete categoria, boolean fastPass, Date fechaFastPass) {
         super(id, categoria, fastPass, fechaFastPass);
     }
+
     @Override
     public boolean esValido(Date fechaUso) {
-        return !utilizado; // solo se puede usar una vez
+        return !utilizado;
     }
-    
 
+    // Persistable
+    @Override
+    public String getTipoEntidad() {
+        return "TiqueteGeneral";
+    }
+
+    @Override
+    public String toString() {
+        return "Tiquete General: " + id + " | Categoría: " + categoria + " | FastPass: " + fastPass;
+    }
 }
